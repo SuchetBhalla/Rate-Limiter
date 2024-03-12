4f2b754b0df973693fe8e85b2cf41ba77302e6fa
Suchet Bhalla <bhalla.suchet@gmail.com>
2024-03-12 21:38:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import threading
 import time
 import os
 import logging
+import queue
 
 # Creates a web-application instance
 app = Flask(__name__)
@@ -23,12 +24,16 @@ requests_lock = threading.Lock()
 # This function removes timestamps, which are older than 60 seconds (for a specific user)
 def clear_expired_requests(user_timestamps):
     now = time.time()
-    tmp = []
-    # Excludes expired timestamps
-    for timestamp in user_timestamps:
-        if now - timestamp < 60:
-            tmp.append(timestamp)
-    return tmp
+
+    while not user_timestamps.empty():
+        # Retrieves the first element in the queue, without removing it
+        timestamp = user_timestamps.queue[0]
+
+        # If it has expired, then remove it
+        if now - timestamp >= 60:
+            user_timestamps.get()
+        else:
+            break
 
 # This function implements rate-limiting with the Sliding Window algorithm
 def allow_request():
@@ -48,18 +53,19 @@ def allow_request():
     with requests_lock:
 
         # Retrieves the (list of) previous time-stamps (for this user)
-        user_timestamps = requests.get(key, [])
+        user_timestamps = requests.get(key, queue.Queue(maxsize= rate_limit))
 
         # Removes timestamps older than 60 seconds (for this user)
-        user_timestamps = clear_expired_requests(user_timestamps)
+        if not user_timestamps.empty():
+            clear_expired_requests(user_timestamps)
 
         # If rate-limit (per minute) has been exceeded
-        if len(user_timestamps) >= rate_limit:
+        if user_timestamps.qsize() >= rate_limit:
             logging.error(f"Rate limit reached by {user_id} @ {ip_address}")
             return jsonify(message= f"Access Denied  to {user_id} @ {ip_address} :: Rate limit reached"), 429
 
         # Else record the current time-stamp
-        user_timestamps.append(time.time())
+        user_timestamps.put( time.time() )
         requests[key] = user_timestamps
 
     return None
